cb413f4deee0fc5a8c96638aa417c97fb908cde9
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-02-09 13:12:15 +0100

    Add startGame source file for actions before game starts

--- js/gameArea.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-	var createGameArea, createIntro, createModalEndWindow;
-
-    createGameArea = function() {
-        let mainArea = document.getElementById('mainGameContainer')
-    }
-};
