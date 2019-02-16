8d0b758e4da18a9a9b83e1b0470bad06e8d2821f
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-02-16 13:20:03 +0100

    Show chosen player img and set player active

--- src/js/eventHandler.js
+++ src/js/eventHandler.js
@@ -5,6 +5,23 @@ const initGame = require('./engine.js').initGame,
     init = require('./engine.js'),
     utils = require('./utils/utils.js');
 
+var startPlayButton = document.querySelectorAll('.modalStart__button')[0];
+choosePlayerEl = document.querySelectorAll('#modalPlayers')[0];
+
+
+// This listner for choosing player body. It sets choosen image 
+// for player and set player property playerActive to true.
+choosePlayerEl.addEventListener('click', function(e) {
+    if (!player.playerBody) {
+        utils.toggleClass(e.target, 'active');
+    } else {
+        utils.toggleClass(player.playerBody, 'active');
+        utils.toggleClass(e.target, 'active');
+    }
+    choosePlayer(e);
+    player.activePlayer();
+});
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
