818c5525f776f7bc0f04f707177c76ce89150903
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-02-23 16:28:49 +0100

    Add methods for Enemy class and fix docs

--- src/js/players/enemy.js
+++ src/js/players/enemy.js
@@ -2,7 +2,7 @@ const Resources = require('../resources/resources.js'),
     utils = require('../utils/utils.js');
 
 // Enemies our player must avoid
-var Enemy = function(x,y,s) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -12,7 +12,10 @@ var Enemy = function(x,y,s) {
     this.x = x;
     this.y = y;
     this.speed = undefined;
+    this.level = 1;
 };
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -20,53 +23,86 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(!this.speed){
+    if (!this.speed) {
         this.setSpeed();
     }
-    this.x += dt * this.speed;
-    if(this.x > 101*6){
+    
+    this.x = this.x + (dt * this.speed);
+
+    if (this.x > 101 * 6) {
         this.x = -100;
         this.setSpeed();
     }
 };
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function(dt) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.getPosition = function() {
-    let left, right, bottom, top;
-    
-    left = this.x;
-    top = this.y + 44;
-    right = this.x + 101;
-    bottom = this.y + 171;
 
+//Return current enemy position
+Enemy.prototype.getPosition = function() {
     return {
-        left: left,
-        right: right,
-        top: top,
-        bottom: bottom
+        x: this.x,
+        y: this.y
     };
 };
 
-Enemy.prototype.setSpeed = function() {
-    let random = Math.random().toFixed(1)*10;
-    random = random >= 5 ? random/2 : random;
-    this.speed = Math.round(random*50);
+
+
+// Increment difficulty level
+Enemy.prototype.incrementLevel = function () {
+    this.level += 1;
 };
 
-var enemy1 = new Enemy(-100, 54);
-var enemy2 = new Enemy(-100, 134);
+
+
+// Increment difficulty level
+Enemy.prototype.decrementLevel = function () {
+    this.level -= 1;
+};
+
+
+
+// Get difficulty level
+Enemy.prototype.getLevel = function(){
+    return this.level;
+};
+
+// Reset all values
+// @param {Number} x Horizontal start position for all enemies
+Enemy.prototype.resetAllValues = function(x){
+    this.x = x;
+    this.speed = undefined;
+    this.level = 1;
+};
+
+// Set speed depends on difficulty level
+Enemy.prototype.setSpeed = function() {
+    let random = Math.random().toFixed(1) * 10;
+    if(this.level === 1){
+        random = random >= 5 ? random / 2 : random;
+    } else if(this.level === 2){
+        random = random >= 5 ? random : random * 2;
+    } else if (this.level === 3){
+        random = random >= 5 ? random * 3 : random * 4;
+    } else {
+        random = random >= 5 ? random * 5 : random * 6;
+    }
+    this.speed = Math.round(random * 50);
+};
+
+
+// Defined enemies
+var enemy1 = new Enemy(-100, 53);
+var enemy2 = new Enemy(-100, 136);
 var enemy3 = new Enemy(-100, 219);
 
-// function createRandomEnemy(){
 
-// }
-
-
-var allEnemies = [enemy1,enemy2,enemy3];
+var allEnemies = [enemy1, enemy2, enemy3];
 
 
 module.exports = {
