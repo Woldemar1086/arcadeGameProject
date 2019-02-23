fbcf3a796cd3cd4b91fb3150fe4d3718da66514d
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-02-23 16:26:21 +0100

    Add listener for restart button

--- src/js/eventHandler.js
+++ src/js/eventHandler.js
@@ -1,12 +1,16 @@
 const initGame = require('./resources/engine.js').initGame,
-    closeModalStart = require('./startGame.js').closeModalStart,
-    choosePlayer = require('./startGame.js').choosePlayer,
-    player = require('./players/player.js').player,
-    init = require('./resources/engine.js'),
-    utils = require('./utils/utils.js');
+    player = require('./players/player.js'),
+    allEnemies = require('./players/enemy.js').allEnemies,
+    utils = require('./utils/utils.js'),
+    gameProcess = require('./players/gameProcess.js');
 
-var startPlayButton = document.querySelectorAll('.modalStart__button')[0];
-choosePlayerEl = document.querySelectorAll('#modalPlayers')[0];
+
+
+var startPlayButton = document.querySelectorAll('.modalStart__button')[0],
+    choosePlayerEl = document.querySelectorAll('#modalPlayers')[0],
+    restartButton = document.querySelectorAll('.modalGameEnd__restartButton')[0];
+
+
 
 
 // This listner for choosing player body. It sets choosen image 
@@ -18,17 +22,36 @@ choosePlayerEl.addEventListener('click', function(e) {
         utils.toggleClass(player.playerBody, 'active');
         utils.toggleClass(e.target, 'active');
     }
-    choosePlayer(e);
+    gameProcess.choosePlayer(e, player);
     player.activePlayer();
 });
 
 
-// This listens for buton start game
+
+
+// On click hide modal start, set gameInProcess to `true`
+// Initialize the game and delete CSS class active from modal window with players
 startPlayButton.addEventListener('click', function(e) {
     if (player.getPlayerActivity()) {
-        closeModalStart();
+        gameProcess.modalStartVisibility();
+        gameProcess.setGameInProcess(true);
+        gameProcess.gameInfoVisibility();
         initGame();
+        utils.toggleClass(document.querySelectorAll('#modalPlayers .active')[0], 'active');
     }
+});
+
+
+// Restart button reset all properties for enemies and player to default.
+// Close the ModalEnd and open start modal window
+restartButton.addEventListener('click', function(e) {
+    player.resetAllValues();
+    allEnemies.forEach(function(enemy) {
+        enemy.resetAllValues(-100);
+    });
+
+    gameProcess.gameEndModal();
+    gameProcess.modalStartVisibility();
 });
 
 // This listens for key presses and sends the keys to your
