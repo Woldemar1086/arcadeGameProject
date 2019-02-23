b77deabe7e3343f4e220114fb6271f4dabfaa074
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-02-23 16:23:28 +0100

    Remove unused file app.js

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-const Resources = require('./resources/resources.js'),
-    utils = require('./utils/utils.js'),
-    enemyCmp = require('./players/enemy.js'),
-    playerCmp = require('./players/player.js');
-
-
-window.playerComponent = playerCmp;
